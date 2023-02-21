15aa9d463b8efab7d7501854d9dec5bbd90df830
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-02-21 15:43:30 -0500

    Init db before app run

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from app.logger import configure_logger
+from app.utils import initialize_db_with_json_data
 
 from config import Config
 from app.extensions import db
@@ -8,15 +10,21 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    configure_logger(app)
+
     # Initialize Flask extensions here
     db.init_app(app)
+    with app.app_context():
+        initialize_db_with_json_data()
 
     # Register blueprints here
-    from app.main import bp as main_bp
-    app.register_blueprint(main_bp)
+    from app.blueprints.users import bp as users_bp
+    from app.blueprints.skills import bp as skills_bp
+    app.register_blueprint(users_bp, url_prefix='/users')
+    app.register_blueprint(skills_bp, url_prefix='/skills')
 
     @app.route('/test/')
     def test_page():
-        return '<h1>Testing the Flask Application Factory Pattern</h1>'
+        return '<h1>Testing the Flask Application</h1>'
 
     return app

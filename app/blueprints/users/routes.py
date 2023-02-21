540a985eb77f8865c11e3a64a579dfbb0b1a6a32
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-02-21 15:42:12 -0500

    Blueprints folder

---
+++ app/blueprints/users/routes.py
@@ -0,0 +1,44 @@
+from flask import jsonify, render_template, request
+from app.models.skill import Skill
+from app.users import bp
+from app.extensions import db
+from app.models.user import User
+
+
+@bp.get('/')
+def get_users():
+    users = User.query.all()
+    user_list = [user.to_dict() for user in users]
+
+    return jsonify(user_list)
+
+
+@bp.get('/<int:id>')
+def get_user(id):
+    user = User.query.get_or_404(id)
+
+    return jsonify(user.to_dict())
+
+
+@bp.put('/<int:id>')
+def update_user(id):
+    user = User.query.get_or_404(id)
+    data = request.get_json()
+
+    # Update non-relationship fields
+    user.name = data.get('name', user.name)
+    user.company = data.get('company', user.company)
+    user.phone = data.get('phone', user.phone)
+
+    # Update skills
+    for skill in data.get('skills', []):
+        user_skill = user.get_skill(skill['skill'])
+        if user_skill:
+            user_skill.rating = skill['rating']
+        else:
+            user.skills.append(
+                Skill(name=skill['skill'], rating=skill['rating']))
+
+    db.session.commit()
+
+    return jsonify(user.to_dict())
